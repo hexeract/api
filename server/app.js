843ef4d5830a34c49c570f295bc9263a4716aad0
Matias Pierobon <matias.pierobonm@gmail.com>
2015-12-13 07:16:13 -0300

    api initial commit

---
+++ server/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var port = process.env.PORT || 1337;
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
